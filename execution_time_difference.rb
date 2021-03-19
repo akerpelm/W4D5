6dfc04d315ba1042d56cdd2b44bb762529c84ad4
akerpelm <akerpelm@u.rochester.edu>
2021-03-19 14:05:14 -0400

    Complete worse subsum

--- execution_time_difference.rb
+++ execution_time_difference.rb
@@ -38,3 +38,36 @@ p my_min(list)  # =>  -5
 puts
 
 p my_worse_min(list)
+
+
+
+#worse
+def largest_contiguous_subsum(list) 
+    subs = [] #O(1)
+    list.each_with_index do |ele_1, i_1| #O(n)
+        list.each_with_index do |ele_2, i_2| #O(n)
+            subs << list[i_1..i_2].sum if i_2 > i_1 #O(n)
+        end
+    end
+    subs.max #O(1)
+end
+#O(1) + #O(n) * #O(n) * O(n) + O(1)
+# = O(n^3) = exponential
+
+#better
+def better_largest_contiguous_subsum(list)
+    #one var tracks largest sum, one var tracks current sum
+    #while loops - two pointers? 
+    #compare l/r put sum into 'sub_sum', increment/decrement
+
+end
+#     list = [2, 3, -6, 7, -6, 7]
+#     largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
+# Example 3:
+
+#     list = [-5, -1, -3]
+#     largest_contiguous_subsum(list) # => -1 (from [-1])
+
+
+list = [5, 3, -7]
+p largest_contiguous_subsum(list) # => 8
