1bff9623e2242293420eb7a36ee3699ed3453629
chrisweeting <chrisweeting@gmail.com>
2021-03-19 13:44:50 -0400

    Wrote my_worse_min

--- execution_time_difference.rb
+++ execution_time_difference.rb
@@ -1,9 +1,29 @@
  
-#  better
+# worse O(n^2)
+def my_worse_min(arr)
+    smallest_num = arr.first #O(1)
+
+    arr.each_with_index do |ele_1, i_1| #O(n)
+        arr.each_with_index do |ele_2, i_2| #O(n)
+            # ele_1 >= ele_2 ? smallest_num = ele_2 : ele_1
+            if ele_1 < smallest_num
+                smallest_num = ele_1
+            elsif ele_2 < smallest_num
+                smallest_num = ele_2
+            else   
+                smallest_num = smallest_num
+            end
+        end
+    end
+
+    smallest_num
+end
+
+#  better O(n)
  def my_min(arr)
      smallest_num = arr.first 
 
-     arr.each do |ele| 
+     arr.each do |ele| #O(n)
          smallest_num = ele if ele < smallest_num
      end
 
@@ -14,3 +34,7 @@
  
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 p my_min(list)  # =>  -5
+
+puts
+
+p my_worse_min(list)
